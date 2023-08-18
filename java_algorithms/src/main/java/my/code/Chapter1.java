def14f283505ff4d56c193901ca72cbb74273ca3
S. Ahmet Bolac <sabolac@gmail.com>
2023-08-18 20:49:57 +0300

    Add more exercises

--- java_algorithms/src/main/java/my/code/Chapter1.java
+++ java_algorithms/src/main/java/my/code/Chapter1.java
@@ -16,7 +16,7 @@ public class Chapter1
     public static void main(String[] args)
     {
         StdOut.println(">>>>>>>>> Start Execution");
-        exercise_1_1_33();
+        exercise_1_1_35();
         StdOut.println(">>>>>>>>> End Execution");
     }
 
@@ -843,5 +843,46 @@ public class Chapter1
                 StdOut.printf("%3.0f ", v);
             StdOut.println();
         }
+    }
+
+    public static void exercise_1_1_35()
+    {
+        // Dice simulation. The following code computes the exact probability
+        // distribution for the sum of two dice:
+        int SIDES = 6;
+        double[] dist = new double[2 * SIDES + 1];
+        for (int i = 1; i <= SIDES; i++)
+            for (int j = 1; j <= SIDES; j++)
+                dist[i + j] += 1.0;
+        for (int k = 2; k <= 2 * SIDES; k++)
+            dist[k] /= 36.0;
+
+        // The value dist[i] is the probability that the dice sum to i. Run experiments
+        // to validate this calculation simulating N dice throws, keeping track of the
+        // frequencies of occurrence of each value when you compute the sum of two
+        // random integers between 1 and 6. How large does N have to be before your
+        // empirical results match the exact results to three decimal places?
+        int N = 1000000;
+        int dice1, dice2;
+        double[] trialResult = new double[2 * SIDES + 1];
+        for (int i = 0; i < N; i++)
+        {
+            dice1 = StdRandom.uniformInt(1, SIDES + 1);
+            dice2 = StdRandom.uniformInt(1, SIDES + 1);
+            trialResult[dice1 + dice2]++;
+        }
+
+        boolean equalsTo3DecimalPlaces = true;
+        for (int i = 2; i <= 2 * SIDES; i++)
+        {
+            trialResult[i] /= N;
+            StdOut.printf("dist=%.3f experiment=%.3f\n", dist[i], trialResult[i]);
+            if (equalsTo3DecimalPlaces && Math.abs(dist[i] - trialResult[i]) >= 0.001)
+            {
+                equalsTo3DecimalPlaces = false;
+            }
+        }
+
+        StdOut.printf("%s\n", equalsTo3DecimalPlaces ? "Match" : "NOT Match");
     }
 }
